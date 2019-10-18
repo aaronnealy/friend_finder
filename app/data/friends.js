5d4b45176dfc249124daace05e3ce49bf5b5576d
Aaron Nealy <50381328+aaronnealy@users.noreply.github.com>
2019-10-18 12:05:50 -0500

    created survey questions as well as file structure and json file

---
+++ app/data/friends.js
@@ -0,0 +1,43 @@
+let friends =
+[{
+    name: "Harry Potter",
+    photo: "https://timedotcom.files.wordpress.com/2014/07/301386_full1.jpg",
+    score: [
+        
+    ]
+},
+{
+    name: "Sabrina Spellman",
+    photo: "https://vignette.wikia.nocookie.net/riverdale/images/1/1b/CAOS-S1-Sabrina-Spellman-Promotional-Portrait-01.jpg/revision/latest/top-crop/width/320/height/320?cb=20181103190011&path-prefix=fr",
+    score:[
+
+    ]
+},
+{
+    name: "Madison Montgomery",
+    photo: "https://mystylenews.com/wp-content/uploads/2018/08/ccelebritiesfotors_600x600-180807105008-600-ahs-coven2.jpg",
+    score:[
+
+    ]
+},
+{
+    name: "Prudence Night",
+    photo: "https://www.thewrap.com/wp-content/uploads/2018/10/Chilling-Advenures-of-Sabrina-Prudence.jpg",
+    score:[
+
+    ]
+},
+{
+    name: "Bonnie Bennett",
+    photo: "https://pmctvline2.files.wordpress.com/2016/04/the-vampire-diaries-kat-graham-leaving.jpg?w=620&h=420&crop=1",
+    score:[
+
+    ]
+},
+{
+    name: "Dr. Strange",
+    photo: "https://hips.hearstapps.com/digitalspyuk.cdnds.net/16/44/1477933942-doctor-strange-movie-composer-cumberbatch.jpg?crop=0.500xw:1.00xh;0.291xw,0&resize=480:*",
+    score:[
+
+    ]
+}]
